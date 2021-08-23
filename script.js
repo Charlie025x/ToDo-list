cf4b6a7c705a42144274a3e16403b97fc7a6bbaa
charlie <charliealonso2001@gmail.com>
2021-08-23 15:21:32 -0500

    lists come from local storage, get lists into the dom

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ newListInput.onkeyup = () => {
         addBtn.classList.remove("active"); // deactivate add button
     }
 }
+
+
+// if user click on the add button
+addBtn.onclick = () => {
+    let userData = newListInput.value; // getting user entered value
+    let getLocalStorage = localStorage.getItem("New Todo"); //getting localStorage
+    if(getLocalStorage == null) { // if localStorage is null
+    listArr = []; //creating a blank array
+    } else {
+        listArr = JSON.parse(getLocalStorage); // transforming json string into a js object
+    }
+    listArr.push(userData); //pushing or adding user data
+    localStorage.setItem("New Todo", JSON.stringify(listArr)); // transforming js object into a json string
+    showTasks() // calling showTasks function
+}
+
+// function to add task list inside ul
+const showTasks = () => {
+    let getLocalStorage = localStorage.getItem("New Todo"); // getting localstorage
+    if(getLocalStorage == null) { // if localStorage is null
+        listArr = []; //creating blank array
+    } else {
+        listArr = JSON.parse(getLocalStorage); // transforming json string into a js object
+    }
+    let newLiTag = '';
+    listArr.forEach((element, index) => {
+        newLiTag += `
+        <li>
+            <span class="check"><i class="fas fa-check"></i></span>
+            ${element}
+            <span class="trash"><i class="fas fa-trash trash"></i></span>
+        </li>
+        `
+    });
+    listContainer.innerHTML = newLiTag; // adding new li tag inside ul tag
+    newListInput.value = ''; // clear input field when new list is created
+}
+
+showTasks();
